8863f029bbc6a2d8a570685aaa38fd085f37cd77
Rasmi Roy <rasmiroy@yahoo.com>
2024-05-21 21:46:46 -0400

    updated the navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,9 +26,9 @@ const AppNavbar = () => {
               {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
                 <>
-                  <Nav.Link as={Link} to='/saved'>
+                  <a  href='/saved' class="nav-link">
                     See Your Books
-                  </Nav.Link>
+                  </a>
                   <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                 </>
               ) : (
